caa8d07e6e046d97c1368673488e722051a48a5a
aerdikov <a.erdikov@gmail.com>
2020-05-23 16:23:06 +0700

    Added custom exceptions + listener for it, added handle for rooms password, added flash messages

---
+++ src/Exception/CustomResponseException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+use App\Services\Response\ErrorCode;
+use Exception;
+use Throwable;
+
+abstract class CustomResponseException extends Exception
+{
+    public function __construct(int $code, ?string $message = null, ?Throwable $previous = null)
+    {
+        if (!$message) {
+            $message = ErrorCode::getMessage($code);
+        }
+
+        parent::__construct((string)$message, $code, $previous);
+    }
+}

d3fcccbc4237326b68e4811aad158eb406a33c5f
ko4a <slavakochnevcase@gmail.com>
2020-06-07 16:24:07 +0700

    cors support added

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];

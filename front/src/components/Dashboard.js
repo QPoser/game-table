9b2e9ce848b3185ee506f9778127a8fa31313852
Sergey <Sergey.Navrockiy@noveogroup.com>
2020-06-07 08:16:30 +0700

    front-init

---
+++ front/src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class Dashboard extends Component {
+
+  render() {
+
+    return (
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+            Games  
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+
+
+export default Dashboard;

2d38c68eb606b365e92660ae2a14ca78618fb0a8
Andrey Erdikov <andreyerdikov@iMac-Andrey.local>
2021-01-10 00:11:56 +0700

    Added psalm and fixed problems for 5 and 6 error lvls

--- src/Repository/Game/Quiz/QuizGameRepository.php
+++ src/Repository/Game/Quiz/QuizGameRepository.php
@@ -8,40 +8,10 @@ use App\Entity\Game\Quiz\QuizGame;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-
 final class QuizGameRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, QuizGame::class);
     }
-
-    // /**
-    //  * @return QuizGame[] Returns an array of QuizGame objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('q.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?QuizGame
-    {
-        return $this->createQueryBuilder('q')
-            ->andWhere('q.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
--- src/Repository/Game/Team/GameTeamPlayerRepository.php
+++ src/Repository/Game/Team/GameTeamPlayerRepository.php
@@ -8,40 +8,10 @@ use App\Entity\Game\Team\GameTeamPlayer;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-
 final class GameTeamPlayerRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, GameTeamPlayer::class);
     }
-
-    // /**
-    //  * @return GameTeamPlayer[] Returns an array of GameTeamPlayer objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('g.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?GameTeamPlayer
-    {
-        return $this->createQueryBuilder('g')
-            ->andWhere('g.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
